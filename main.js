35c40b6f17d5ddad2f201d709d3fa8ae5ab3a3cb
Vedha-A <85233825+Vedha-A@users.noreply.github.com>
2023-03-21 16:28:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function setup()
 	instializeInSetup(mario);
 	video = createCapture(VIDEO);
 	video.size(600, 300);
-	video.parent('game console');
+	video.parent('game_console');
 
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
