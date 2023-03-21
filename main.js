67c1dc9e6e91113ae4a0dddafbd3aa1ec6883566
Vedha-A <85233825+Vedha-A@users.noreply.github.com>
2023-03-21 17:06:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,15 +12,18 @@ function preload() {
 
 function setup() 
 {
-	canvas = createCanvas(1240, 336);
+	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
+
 	instializeInSetup(mario);
+	
 	video = createCapture(VIDEO);
-	video.size(800, 400);
+	video.size(800,400);
 	video.parent('game_console');
 
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
+
 }
 
 function modelLoaded() 
